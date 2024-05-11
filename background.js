90eddf9d7578ab52d50dc6fc5193bd49b6e12e8c
soham0w0sarkar <sohamsarkar59@gmail.com>
2024-05-11 15:52:59 +0530

    added setting to not open the app while rdirecting

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
 chrome.tabs.onCreated.addListener(function (tab) {
-  chrome.tabs.update(tab.id, { url: "https://zen-flow-frontend.vercel.app/" });
+  console.log(tab);
+  if (tab.status == "loading" && tab.pendingUrl == "chrome://newtab/") {
+    console.log("New tab created");
+    chrome.tabs.update(tab.id, {
+      url: "https://zen-flow-frontend.vercel.app",
+    });
+  }
+  console.log("Tab created");
 });

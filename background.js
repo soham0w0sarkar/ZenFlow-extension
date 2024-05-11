49812eb654dce0512d6a21b7ee75f2d4a18dea74
soham0w0sarkar <sohamsarkar59@gmail.com>
2024-05-11 15:55:40 +0530

    removing test logs

--- background.js
+++ background.js
@@ -1,10 +1,7 @@
 chrome.tabs.onCreated.addListener(function (tab) {
-  console.log(tab);
   if (tab.status == "loading" && tab.pendingUrl == "chrome://newtab/") {
-    console.log("New tab created");
     chrome.tabs.update(tab.id, {
       url: "https://zen-flow-frontend.vercel.app",
     });
   }
-  console.log("Tab created");
 });
